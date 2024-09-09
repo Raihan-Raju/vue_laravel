0be7e76e3cf4cd1c9137749c73366027c5f4460e
Raihan-Raju <idbraihan58@gmail.com>
2024-09-09 19:02:28 +0600

    first commit

---
+++ tuhins/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import Navber from './layouts/Navber.vue';
+
+
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+     
+      <Navber />
+     
+    </div>
+  </header>
+</template>
+
+<RouterView />
+<style scoped>
+
+</style>
